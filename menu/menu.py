a703275f745e93627a53173c618bdf8b007fb34b
Peter <plarochkin@gmail.com>
2022-05-14 19:34:27 +0300

    feat: add saving, read, serialize menu_object

--- menu/menu.py
+++ menu/menu.py
@@ -1,72 +1,53 @@
-from zoneinfo import available_timezones
+# from zoneinfo import available_timezones
+from __future__ import annotations
 import pygame 
+from typing import List
+from pydantic import BaseModel, ValidationError, Field, validator
+import json
 
-            
+
+# https://stackoverflow.com/questions/68091480/definining-recursive-models-in-pydantic
+class BaseMenu(BaseModel):
+    name: str
+    node_type: str = Field(alias="node_type")
+    nodes: List[BaseMenu] = []
+    
+    max_value: int = None
+    # @validator('max_value')
+    # def prevent_max_value(cls, v):
+    #     assert v is not None, 'size may not be None'
+    #     return v
+
+    value: int = None
+    # @validator('value')
+    # def prevent_value(cls, v):
+    #     assert v is not None, 'size may not be None'
+    #     return v
+
+    values: list[str] = None
+    # @validator('values')
+    # def prevent_values(cls, v):
+    #     assert v is not None, 'size may not be None'
+    #     return v
+    
+BaseMenu.update_forward_refs()
 
 
-MENU_JSON = {
-    "name" : "Menu",
-    "nodeType": "Menu",
-    "nodes" :[
-        {   
-            "name" : "Start",
-            "nodeType": "button",
-            "nodes" : []
-        },
-        {   
-            "name" : "Options",
-            "nodeType": "Menu",
-            "nodes" : [
-                {   
-                    "name" : "Volume",
-                    "nodeType": "Slider",
-                    "maxValue" : 20,
-                    "value": 10,
-                    "function": lambda x: print(x)
-                    # "nodes" : []
-                },
-                {   
-                    "name" : "Music",
-                    "nodeType": "Turner",
-                    "value": 0,
-                    "values": ["hello", "my", "friend"],
-                    "function": lambda x: print(x)
-                    # "nodes" : []
-                },
-                {   
-                    "name" : "Credits",
-                    "nodeType": "Menu",
-                    
-                    "nodes" : [
-                                    {   
-                                "name" : "Made by Sasha and Petya",
-                                "nodeType": "Menu",
-                                "nodes" : []
-                            }
-                    ]
-                }
-            ]
-        },
-        {   
-            "name" : "Credits",
-            "nodeType": "Text",
-            "text" : "Hello",
-            "nodes" : []
-        }
-    ]
-}
+
+
+
 
 class FreeMenu():
-    def __init__(self, menu_json, DISPLAY_W, DISPLAY_H):
-        self.current_path = []
+    def __init__(self, menu_json: BaseMenu, DISPLAY_W, DISPLAY_H):
         self.menu_json = menu_json
+        self.current_path = [self.menu_json]
         self.current_json = self.menu_json
         self.run_display = True
         self.playing = True
         self.offset = -100
-        self.state = "Start"
+        self.state = self.menu_json.name
         self.num_item = 0
-        self.font_name = "./COMIC.TTF"
+        self.font_name = "menu/COMIC.TTF"
         self.cursor_rect = pygame.Rect(0, 0, 20, 20)
         self.display_w, self.display_h = DISPLAY_W, DISPLAY_H
         self.display = pygame.Surface((self.display_w, self.display_h))
@@ -77,10 +58,10 @@ class FreeMenu():
         self.creditsx, self.creditsy = self.mid_w, self.mid_h + 70
         self.BLACK, self.WHITE, self.GRAY = (0,0,0), (255,255,255), (229, 229, 229)
         self.window = pygame.display.set_mode((self.display_w, self.display_h))
-        self.current_header = self.menu_json["name"]
+        self.current_header = self.menu_json.name
         self.left_edge = self.display_w / 2
         self.available_height = self.display_h / 2 + 20
-        self.size_between_elements = self.available_height / len(self.current_json)
+        self.size_between_elements = self.available_height / len(self.current_json.__fields__)
 
     def draw_cursor(self):
         self.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
@@ -114,17 +95,17 @@ class FreeMenu():
             self.display.fill(self.GRAY)
             self.draw_text(self.current_header, 20, self.display_w / 2, self.display_h / 2 - 40)
             
-            for i, element in enumerate(self.current_json["nodes"]):
-                if element["nodeType"] == "Turner":
-                    self.draw_text(element["name"] + ":{}".format(element["values"][element["value"]]), 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
-                elif element["nodeType"] == "Slider":
-                    self.draw_text(element["name"] + ":{}".format("X" * element["value"]), 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
+            for i, element in enumerate(self.current_json.nodes):
+                if element.node_type == "Turner":
+                    self.draw_text(element.name + ":{}".format(element.values[element.value]), 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
+                elif element.node_type == "Slider":
+                    self.draw_text(element.name + ":{}".format("X" * element.value), 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
                 else:
-                    self.draw_text(element["name"], 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
+                    self.draw_text(element.name, 20, self.left_edge, self.display_h / 2 + self.size_between_elements * i)
             # self.draw_text("Options", 20, self.optionsx, self.optionsy)
             # self.draw_text("Credits", 20, self.creditsx, self.creditsy)
-            if len(self.current_json["nodes"])!=0:
-                if (len(self.current_json["nodes"])) - 1 < self.num_item :
+            if len(self.current_json.nodes)!=0:
+                if (len(self.current_json.nodes)) - 1 < self.num_item :
                     self.num_item = 0
                 self.draw_cursor()
             self.blit_screen()
@@ -134,20 +115,21 @@ class FreeMenu():
         if self.START_KEY:
             if self.state == "Start":
                 self.playing = True
+                self.save_settings()
+                self.run_display = False
             else:
-                if self.current_json["nodes"][self.num_item]["nodeType"] == "Menu":
-                    self.current_path.append(self.num_item)
-                    self.current_json = self.current_json["nodes"][self.num_item]
-                    self.current_header = self.current_json["name"]
+                if self.current_json.nodes[self.num_item].node_type == "Menu":
+                    self.current_path.append(self.current_json)
+                    self.current_json = self.current_json.nodes[self.num_item]
+                    self.current_header = self.current_json.name
                 
-
-        if self.BACK_KEY: 
+        elif self.BACK_KEY: 
             if len(self.current_path) != 0:
-                menu_item = self.current_path.pop()
-                self.current_json = self.menu_json
-                for item in self.current_path:
-                    self.current_json = self.current_json["nodes"][item]
-                self.current_header = self.current_json["name"]
+                # menu_item = self.current_path.pop()
+                self.current_json = self.current_path.pop()
+                # for item in self.current_path:
+                #     self.current_json = self.current_json["nodes"][item]
+                self.current_header = self.current_json.name
                 # self.num_item = 0
 
         
@@ -165,51 +147,69 @@ class FreeMenu():
     
     def move_cursor(self):
         # i = 0
-        if (len(self.current_json["nodes"])) - 1 < self.num_item :
+        if (len(self.current_json.nodes)) - 1 < self.num_item :
             self.num_item = 0
         if self.DOWN_KEY:
             self.num_item += 1 
-            self.num_item = self.num_item % (len(self.current_json["nodes"]))
+            self.num_item = self.num_item % (len(self.current_json.nodes))
             i = self.num_item
-            self.state = self.current_json["nodes"][self.num_item]["name"]
+            self.state = self.current_json.nodes[self.num_item].name
             self.draw_text("*", 20, self.left_edge, self.display_h / 2 + self.size_between_elements * self.num_item)
             self.cursor_rect.midtop = (self.left_edge + self.offset, self.display_h / 2 + self.size_between_elements * self.num_item)
         
         elif self.UP_KEY:
             self.num_item -= 1 
             if self.num_item < 0:
-                self.num_item = (len(self.current_json["nodes"])) - 1
+                self.num_item = (len(self.current_json.nodes)) - 1
             i = self.num_item
-            self.state = self.current_json["nodes"][self.num_item]["name"]
+            self.state = self.current_json.nodes[self.num_item].name
             self.draw_text("*", 20, self.left_edge, self.display_h / 2 + self.size_between_elements * self.num_item)
             self.cursor_rect.midtop = (self.left_edge + self.offset, self.display_h / 2 + self.size_between_elements * self.num_item)
         
         if self.LEFT_KEY: 
-            if self.current_json["nodes"][self.num_item]["nodeType"] == "Slider":
-                if self.current_json["nodes"][self.num_item]["value"] == 0:
-                    self.current_json["nodes"][self.num_item]["value"] = self.current_json["nodes"][self.num_item]["maxValue"]
+            if self.current_json.nodes[self.num_item].node_type == "Slider":
+                if self.current_json.nodes[self.num_item].value == 0:
+                    self.current_json.nodes[self.num_item].value = self.current_json.nodes[self.num_item].max_value
                 else:
-                    self.current_json["nodes"][self.num_item]["value"] -= 1
-            elif self.current_json["nodes"][self.num_item]["nodeType"] == "Turner":  
-                if self.current_json["nodes"][self.num_item]["value"] == 0:
-                    self.current_json["nodes"][self.num_item]["value"] = len(self.current_json["nodes"][self.num_item]["values"]) - 1
+                    self.current_json.nodes[self.num_item].value -= 1
+            elif self.current_json.nodes[self.num_item].node_type == "Turner":  
+                if self.current_json.nodes[self.num_item].value == 0:
+                    self.current_json.nodes[self.num_item].value = len(self.current_json.nodes[self.num_item].values) - 1
                 else:
-                    self.current_json["nodes"][self.num_item]["value"] -= 1
+                    self.current_json.nodes[self.num_item].value -= 1
 
         elif self.RIGHT_KEY: 
-            if self.current_json["nodes"][self.num_item]["nodeType"] == "Slider":
-                self.current_json["nodes"][self.num_item]["value"] += 1
-                self.current_json["nodes"][self.num_item]["value"] = self.current_json["nodes"][self.num_item]["value"] % self.current_json["nodes"][self.num_item]["maxValue"]
-            elif self.current_json["nodes"][self.num_item]["nodeType"] == "Turner":  
-                self.current_json["nodes"][self.num_item]["value"] += 1
-                self.current_json["nodes"][self.num_item]["value"] = self.current_json["nodes"][self.num_item]["value"] % len(self.current_json["nodes"][self.num_item]["values"])
+            if self.current_json.nodes[self.num_item].node_type == "Slider":
+                self.current_json.nodes[self.num_item].value += 1
+                self.current_json.nodes[self.num_item].value = self.current_json.nodes[self.num_item].value % self.current_json.nodes[self.num_item].max_value
+            elif self.current_json.nodes[self.num_item].node_type == "Turner":  
+                self.current_json.nodes[self.num_item].value += 1
+                self.current_json.nodes[self.num_item].value = self.current_json.nodes[self.num_item].value % len(self.current_json.nodes[self.num_item].values)
+
+        if self.BACK_KEY: 
+            if len(self.current_path) != 0:
+                # menu_item = self.current_path.pop()
+                self.current_json = self.current_path.pop()
+                # for item in self.current_path:
+                #     self.current_json = self.current_json["nodes"][item]
+                self.current_header = self.current_json.name
+                # self.num_item = 0
 
     def blit_screen(self):
         self.window.blit(self.display, (0, 0))
         pygame.display.update()
         self.reset_keys()
+    
+    def save_settings(self):
+        f = open("menu/settings.json", "w")
+        f.write(self.menu_json.json())
+        f.close()
 
-pygame.init()
-menu = FreeMenu(MENU_JSON, 480, 270)
-
-menu.display_menu()
+try:
+    setting_string = open("menu/settings.json", "r").read()
+    menu_object = BaseMenu.parse_raw(setting_string)
+    pygame.init()
+    menu = FreeMenu(menu_object, 480, 270)
+    menu.display_menu()
+except ValidationError as e:
+    print(e.json())

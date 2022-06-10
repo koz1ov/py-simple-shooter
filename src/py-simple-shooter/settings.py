cc7e1003115d500eb625a4bea9f94d07ae10693b
Peter <plarochkin@gmail.com>
2022-06-11 02:42:22 +0300

    docs settings: add some docstrings

--- src/py-simple-shooter/settings.py
+++ src/py-simple-shooter/settings.py
@@ -3,13 +3,16 @@ path_to_menu = "/".join(str(__file__).split("/")[:-1]) + "/"
 
 
 class SettingsData(BaseModel):
+    """Base class for serializations json data from settings-json-file."""
     volume: int = Field(alias="volume")
     music: int = Field(alias="music")
     language: int = Field(alias="language")
 
 
 class Settings():
+    """Class handler, which accumulate settings data and save this data to file."""
     def __init__(self, path: str = path_to_menu + "menu/settings.json") -> None:
+        """Init data: read settings-json-file and keep itself."""
         self.path = path
         file = open(self.path, "r")
         setting_string = file.read()
@@ -17,6 +20,7 @@ class Settings():
         file.close()
 
     def save(self):
+        """Function for save actual setting to settings-json-file."""
         file = open(path_to_menu + "menu/settings.json", "w")
         file.write(self.data.json())
         file.close()
